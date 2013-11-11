dbfebdf55fe7aa701c025ad20c1e5202cd0ab895
Brandon Bloom <brandon@brandonbloom.name>
2013-11-11 17:10:06 -0500

    Even more perl hackery to fix up yaml files

--- down.sh
+++ down.sh
@@ -4,8 +4,7 @@ set -e
 
 crowdin-cli download
 
-# Rewrite locale keys in Yaml files to be quoted.
-# Otherwise Norwegian's `no: 1` is parsed as `{false=>1}`.
+# Fix-up Yaml files.
 for yml in $(find locales -name '*.yml' | grep -v en-US); do
-  perl -i -pe 's/^([a-z]+(?:-[A-Z]+)?):(.*)/"\1":\2/g' $yml
+  perl -i ./lib/fix-yml.pl $yml
 done
