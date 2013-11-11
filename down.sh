7cfc4bc39378220a8fafac73d6cae3e7ec5310f3
Brandon Bloom <brandon@brandonbloom.name>
2013-11-11 12:10:51 -0500

    Fixup top-level keys in down.sh

--- down.sh
+++ down.sh
@@ -3,3 +3,9 @@
 set -e
 
 crowdin-cli download
+
+# Rewrite locale keys in Yaml files to be quoted.
+# Otherwise Norwegian's `no: 1` is parsed as `{false=>1}`.
+for yml in $(find locales -name '*.yml' | grep -v en-US); do
+  perl -i -pe 's/^([a-z]+(?:-[A-Z]+)):(.*)/"\1":\2/g' $yml
+done
