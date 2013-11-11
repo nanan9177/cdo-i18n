5954a057c8ddde0e7c46d6848f9e0598f8151fa6
Brandon Bloom <brandon@brandonbloom.name>
2013-11-11 12:54:10 -0500

    down with fixed key rewriting regex

--- down.sh
+++ down.sh
@@ -7,5 +7,5 @@ crowdin-cli download
 # Rewrite locale keys in Yaml files to be quoted.
 # Otherwise Norwegian's `no: 1` is parsed as `{false=>1}`.
 for yml in $(find locales -name '*.yml' | grep -v en-US); do
-  perl -i -pe 's/^([a-z]+(?:-[A-Z]+)):(.*)/"\1":\2/g' $yml
+  perl -i -pe 's/^([a-z]+(?:-[A-Z]+)?):(.*)/"\1":\2/g' $yml
 done
